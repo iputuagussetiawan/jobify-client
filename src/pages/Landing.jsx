cb558ce7487ea563f6ce875fd962125fce1840cb
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-03-03 14:44:48 +0800

    styled component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBtn=styled.button`
+  font-size: 32px;
+  background-color: red;
+  color: white;
+`
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <h1>Landing</h1>
+      <StyledBtn> Read More</StyledBtn>
+    </div>
   )
 }
 
