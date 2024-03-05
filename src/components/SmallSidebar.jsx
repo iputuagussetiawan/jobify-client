143c44d21dbdaf83392560c21e6cf9c3015e1512
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-03-05 21:25:25 +0800

    add big sidebar

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,9 +1,10 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { FaTimes } from 'react-icons/fa';
 import Logo from './Logo';
-import { NavLink } from 'react-router-dom';
+
 import links from '../utils/links';
 import { useDashboardContext } from '../pages/DashboardLayout';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
     const {showSidebar, toggleSidebar} = useDashboardContext();
@@ -19,23 +20,7 @@ const SmallSidebar = () => {
                 <header>
                     <Logo />
                 </header>
-                <div className='nav-links'>
-                    {links.map((link) => {
-                        const { text, path, icon } = link;
-                        return (
-                            <NavLink
-                                to={path}
-                                key={text}
-                                className='nav-link'
-                                onClick={toggleSidebar}
-                                end
-                            >
-                                <span className='icon'>{icon}</span>
-                                {text}
-                            </NavLink>
-                        );
-                    })}
-                </div>
+                <NavLinks/>
             </div>
         </div>
         </Wrapper>
