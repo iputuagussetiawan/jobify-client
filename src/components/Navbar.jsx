e16401b010c8e739a7f1acfe884550379116d624
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-03-08 20:35:22 +0800

    logout button and toggle dark and light theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { FaAlignLeft } from 'react-icons/fa';
 import Logo from './Logo';
 
 import { useDashboardContext } from '../pages/DashboardLayout';
+import LogoutContainer from './LogoutContainer';
+import ThemeToggle from './ThemeToggle';
 const Navbar = () => {
     const { toggleSidebar } = useDashboardContext();
     return (
@@ -15,7 +17,8 @@ const Navbar = () => {
                 <Logo />
                 <h4 className='logo-text'>dashboard</h4>
             </div>
-            <div className='btn-container'>toggle/logout</div>
+            <ThemeToggle/>
+            <LogoutContainer/>
         </div>
         </Wrapper>
     );
