513ce34fafb2a920abf89fb55435b9ba3d821aac
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-03-08 21:02:42 +0800

    dark mode and light mode functionality

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Wrapper from '../assets/wrappers/Dashboard'
 import { BigSidebar, Navbar, SmallSidebar } from '../components'
-
 import { useState, createContext, useContext } from 'react';
 const DashboardContext=createContext();
 
-const DashboardLayout = () => {
+const DashboardLayout = ({isDarkThemeEnabled}) => {
   const user = { name: 'agus s' };
-
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
 
   const toggleDarkTheme = () => {
-    console.log('toggle dark theme');
+    const newDarkTheme=!isDarkTheme
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme)
   };
 
   const toggleSidebar = () => {
